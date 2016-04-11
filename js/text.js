f8b6a330bdab3deaf4dc5293bc66412d904d8f97
Eric Jiang <ericjiang97@gmail.com>
2016-04-11 10:30:59 +1000

    beta-version deployment
    beta-version deployment to forked.

--- js/text.js
+++ js/text.js
@@ -20,4 +20,24 @@ setTimeout(function(){
 	});
     }, 2000);
 
+function saveTextAsFile(){      
+    var textToWrite = programCodeMirror.getValue();
+    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+    var fileNameToSaveAs = "myNewFile.txt";
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    window.URL = window.URL || window.webkitURL;
+          
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    
+    downloadLink.click();
+}
 
+function destroyClickedElement(event)
+{
+// remove the link from the DOM
+    document.body.removeChild(event.target);
+}
