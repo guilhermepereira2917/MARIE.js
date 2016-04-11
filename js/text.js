ee26415389a85b47018bd0f0343eb5cd947a1df0
Eric Jiang <ericjiang97@gmail.com>
2016-04-11 16:48:28 +1000

    bugfixes
    bugfixes

--- js/text.js
+++ js/text.js
@@ -1,43 +1,15 @@
-setTimeout(function(){
-	var fileInput = document.getElementById('fileInput');
-	
-	fileInput.addEventListener('change', function(e) {
-		var file = fileInput.files[0];
-		var textType = /text.*/;
-
-		if (file.type.match(textType)) {
+window.onload = function() {
+		var fileInput = document.getElementById('fileInput');
+		fileInput.addEventListener('change', function(e) {
+			var file = fileInput.files[0];
+			
+			
 			var reader = new FileReader();
 
 			reader.onload = function(e) {
 				programCodeMirror.setValue(reader.result);
-				CodeMirror.refresh();
 			}
-
-			reader.readAsText(file);	
-		} else {
-			CodeMirror.refresh(); 
-		}
-	});
-    }, 2000);
-
-function saveTextAsFile(){      
-    var textToWrite = programCodeMirror.getValue();
-    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
-    var fileNameToSaveAs = "myNewFile.txt";
-    var downloadLink = document.createElement("a");
-    downloadLink.download = fileNameToSaveAs;
-    window.URL = window.URL || window.webkitURL;
-          
-    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-    downloadLink.onclick = destroyClickedElement;
-    downloadLink.style.display = "none";
-    document.body.appendChild(downloadLink);
-    
-    downloadLink.click();
-}
-
-function destroyClickedElement(event)
-{
-// remove the link from the DOM
-    document.body.removeChild(event.target);
+			reader.readAsText(file);
+			
+		});
 }
