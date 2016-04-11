cbd4dfe9f5af4913cb254edd59f98a027d6065b9
Jason N <admin@cyderize.org>
2016-04-11 19:10:34 +1000

    Slightly more technically correct syntax highlighting
    Fixes problems with the END keyword since end is a valid label

--- js/syntax.js
+++ js/syntax.js
@@ -1,15 +1,24 @@
 CodeMirror.defineSimpleMode("marie", {
     start: [
-        {regex: /[^\d,\/\s][^,\/\s]*,/, token: "string"},
-        {regex: /end\b/i, token: "keyword", next: "end"},
-        {regex: /(?:add|subt|addi|clear|load|loadi|store|storei|input|output|jump|skipcond|jns|jumpi|halt)\b/i, token: "keyword"},
+        {regex: /[^\d,\/\s][^,\/\s]*,/, token: "string", next: "operator"}, // Labels
+        {regex: /(?:add|subt|addi|load|loadi|store|storei|jump|skipcond|jns|jumpi)\b/i, token: "keyword", next: "operand"}, // Operator
+        {regex: /(?:clear|input|output|halt)\b/i, token: "keyword", next: "start"}, // Operator
         {regex: /(?:org|dec|oct|hex)\b/i, token: "atom", next: "literal"},
-        {regex: /\d[0-9a-f]*\b/i, token: "variable-3", next: "start"},
-        {regex: /[0-9a-z]+\b/i, token: "variable-2", next: "start"},
-        {regex: /\/.*/, token: "comment"},
+        {regex: /\/.*/, token: "comment"}, // Comments
+        {regex: /end\b/i, token: "comment", next: "end"},
+    ],
+    operator: [
+        {regex: /(?:add|subt|addi|clear|load|loadi|store|storei|input|output|jump|skipcond|jns|jumpi|halt)\b/i, token: "keyword", next: "operand"}, // Operator
+        {regex: /(?:add|subt|addi|clear|load|loadi|store|storei|input|output|jump|skipcond|jns|jumpi|halt)\b/i, token: "keyword", next: "start"}, // Operator
+        {regex: /(?:org|dec|oct|hex)\b/i, token: "atom", next: "literal"}, // Literal
+    ],
+    operand: [
+        {regex: /(?:org|dec|oct|hex)\b/i, token: "atom", next: "literal"}, // Literal
+        {regex: /\d[0-9a-f]*\b/i, token: "variable-3", next: "start"}, // Address
+        {regex: /[0-9a-z]+\b/i, token: "variable-2", next: "start"}, // Reference
     ],
     literal: [
-        {regex: /[0-9a-f]+/i, token: "number", next: "start"}
+        {regex: /[0-9a-f]+/i, token: "number", next: "start"} // Number
     ],
     end: [
         {regex: /.*/, token: "comment"}
