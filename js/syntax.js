145cc3482b8f639a9876961e9438d1fec385b415
Eric Jiang <ericjiang97@gmail.com>
2016-04-12 12:30:29 +1000

    Beta Version 2 Release
    - bugfixes
    - implemented *.mas downloading
    - changes to file uploading system
    - imported jQuery.js and FileSaver.js
    - inserted LICENSEs
    - changes to formatting for all webpages
    - updated code to V2 Beta

--- js/syntax.js
+++ js/syntax.js
@@ -1,24 +1,15 @@
 CodeMirror.defineSimpleMode("marie", {
     start: [
-        {regex: /[^\d,\/\s][^,\/\s]*,/, token: "string", next: "operator"}, // Labels
-        {regex: /(?:add|subt|addi|load|loadi|store|storei|jump|skipcond|jns|jumpi)\b/i, token: "keyword", next: "operand"}, // Operator
-        {regex: /(?:clear|input|output|halt)\b/i, token: "keyword", next: "start"}, // Operator
+        {regex: /[^\d,\/\s][^,\/\s]*,/, token: "string"},
+        {regex: /end\b/i, token: "keyword", next: "end"},
+        {regex: /(?:add|subt|addi|clear|load|loadi|store|storei|input|output|jump|skipcond|jns|jumpi|halt)\b/i, token: "keyword"},
         {regex: /(?:org|dec|oct|hex)\b/i, token: "atom", next: "literal"},
-        {regex: /\/.*/, token: "comment"}, // Comments
-        {regex: /end\b/i, token: "comment", next: "end"},
-    ],
-    operator: [
-        {regex: /(?:add|subt|addi|clear|load|loadi|store|storei|input|output|jump|skipcond|jns|jumpi|halt)\b/i, token: "keyword", next: "operand"}, // Operator
-        {regex: /(?:add|subt|addi|clear|load|loadi|store|storei|input|output|jump|skipcond|jns|jumpi|halt)\b/i, token: "keyword", next: "start"}, // Operator
-        {regex: /(?:org|dec|oct|hex)\b/i, token: "atom", next: "literal"}, // Literal
-    ],
-    operand: [
-        {regex: /(?:org|dec|oct|hex)\b/i, token: "atom", next: "literal"}, // Literal
-        {regex: /\d[0-9a-f]*\b/i, token: "variable-3", next: "start"}, // Address
-        {regex: /[0-9a-z]+\b/i, token: "variable-2", next: "start"}, // Reference
+        {regex: /\d[0-9a-f]*\b/i, token: "variable-3", next: "start"},
+        {regex: /[0-9a-z]+\b/i, token: "variable-2", next: "start"},
+        {regex: /\/.*/, token: "comment"},
     ],
     literal: [
-        {regex: /[0-9a-f]+/i, token: "number", next: "start"} // Number
+        {regex: /[0-9a-f]+/i, token: "number", next: "start"}
     ],
     end: [
         {regex: /.*/, token: "comment"}
