36dbf6155250855550725a4ef1cb29b68eb7ecf6
Saurabh Joshi <sauj123@hotmail.com>
2016-04-20 23:19:20 +1000

    moved all source code to the src directory; running 'grunt' will now copy files from the src directory to the build directory, and open index.html in the default browser.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,19 +4,31 @@ module.exports = function(grunt) {
     grunt.initConfig({
         open: {
             dev: {
-                path: 'index.html'
+                path: 'build/index.html'
+            }
+        },
+        copy: {
+            main: {
+                expand: true,
+                cwd: 'src/',
+                src: ['**'],
+                dest: 'build/'
             }
         },
         jshint: {
             files: ['js/*.js', 'Gruntfile.js']
         },
+        clean: ['build'],
         pkg: grunt.file.readJSON('package.json')
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-ejs');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask('build', []);
+    grunt.registerTask('build', ['clean', 'copy']);
     grunt.registerTask('run', ['open:dev']);
     grunt.registerTask('bar', ['build', 'run']);
     grunt.registerTask('default', ['bar']);
