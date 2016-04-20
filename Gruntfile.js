6a4ad281d6e93ab5d43f4d305c9a1beeb486b13c
Saurabh Joshi <sauj123@hotmail.com>
2016-04-21 08:31:34 +1000

    now using ejs for templating documents

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,31 +2,45 @@
 
 module.exports = function(grunt) {
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         open: {
             dev: {
                 path: 'build/index.html'
+            }
+        },
+        ejs: {
+            all: {
+                options: {
+                    version: "<%= pkg.version %>"
+                },
+                expand: true,
+                cwd: 'src/templates/',
+                src: ['*.ejs'],
+                dest: 'build/',
+                ext:  '.html'
+                
             }
         },
         copy: {
             main: {
                 expand: true,
                 cwd: 'src/',
-                src: ['**'],
+                src: ['**/*', '!**/templates/**'],
                 dest: 'build/'
             }
         },
         jshint: {
             files: ['js/*.js', 'Gruntfile.js']
         },
-        clean: ['build'],
-        pkg: grunt.file.readJSON('package.json')
+        clean: ['build']
     });
 
     grunt.loadNpmTasks('grunt-open');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-ejs');
 
-    grunt.registerTask('build', ['clean', 'copy']);
+    grunt.registerTask('build', ['clean', 'ejs', 'copy']);
     grunt.registerTask('run', ['open:dev']);
     grunt.registerTask('bar', ['build', 'run']);
     grunt.registerTask('default', ['bar']);
