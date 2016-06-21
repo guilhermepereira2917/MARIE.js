e15a4b0e2178a1af53719b5aa719a6df0233f83f
Eric Jiang <ericjiang97@gmail.com>
2016-06-21 12:30:52 +1000

    [P] patch

--- src/js/interface.js
+++ src/js/interface.js
@@ -585,7 +585,7 @@ window.addEventListener("load", function() {
 
      $("#download").click( function(){
         var text = programCodeMirror.getValue();
-        var filename = code;
+        var filename = "code";
         var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
         saveAs(blob, filename+".mas");
     });
@@ -605,7 +605,7 @@ window.addEventListener("load", function() {
     });
     $("#saveFile").click( function() {
         var text = programCodeMirror.getValue();
-        var filename = code;
+        var filename = "code";
         var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
         saveAs(blob, filename+".mas");
     });
