3ebe1a48c0230458a2b728e90606af804e610d9a
lorderikir <ejia6@student.monash.edu>
2016-07-04 11:37:11 +1000

    modified some things for jQuery event to work

--- src/js/tour.js
+++ src/js/tour.js
@@ -3,32 +3,33 @@ $(document).ready( function(){
     if (localStorage.getItem("tourComplete") === null) {
         localStorage.setItem("tourComplete", false);
     }
-});
 
-$("#tour").click( function() {
+    $("#starttour").click( function() {
     //if localStorage item is equal to false excute tour code
-    if (localStorage.getItem("tourComplete") === false) {
-        // Instance the tour
-        var tour = new Tour();
-        
-        tour.addsteps([
-            {
-                element: "#program-container",
-                title: "Coding Area",
-                content: "Type in your code here"
-            },
-            {
-                element: "#register container",
-                title: "Registers",
-                content: "This shows you the register values"
-            }
-            ]);
+        if (localStorage.getItem("tourComplete") == false) {
+            // Instance the tour
+            var tour = new Tour();
+            
+            tour.addsteps([
+                {
+                    element: "#program-container",
+                    title: "Coding Area",
+                    content: "Type in your code here"
+                },
+                {
+                    element: "#register container",
+                    title: "Registers",
+                    content: "This shows you the register values"
+                }
+                ]);
 
-        // Initialize the tour
-        tour.init();
+            // Initialize the tour
+            tour.init();
 
-        // Start the tour
-        tour.start();
-    }
+            // Start the tour
+            tour.start();
+        }
 
+    });
 });
+
