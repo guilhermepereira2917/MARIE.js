989052036440e1ee89153672f9c0033705c8612c
Eric Jiang <ejia6@student.monash.edu>
2016-08-16 20:34:46 +1000

    manual update: incorrect extension fix

--- src/js/interface.js
+++ src/js/interface.js
@@ -1153,7 +1153,7 @@ window.addEventListener("load", function() {
         var text = programCodeMirror.getValue();
         var filename = "code";
         var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
-        saveAs(blob, filename+".asmx");
+        saveAs(blob, filename+".mas");
     });
 
     $("#newfilebtn").click(function() {
