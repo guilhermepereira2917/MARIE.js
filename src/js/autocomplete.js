9a3e4e4a56cdf8f0139788faf04abc28c8f2ae56
lorderikir <ejia6@student.monash.edu>
2016-09-13 12:33:21 +1000

    g

--- src/js/autocomplete.js
+++ src/js/autocomplete.js
@@ -1,89 +1,89 @@
 /* exported getCompletions */
 
 function getCompletions(editor) {
-    "use strict";
+  'use strict';
 
-    var cursor = editor.getCursor();
-    var line = editor.getLine(cursor.line);
+  const cursor = editor.getCursor();
+  const line = editor.getLine(cursor.line);
 
-    var containsForwardSlash = line.indexOf("/") >= 0;
+  const containsForwardSlash = line.indexOf('/') >= 0;
 
-    var end = cursor.ch;
-    var start = end;
+  const end = cursor.ch;
+  let start = end;
 
     // move start backwards until it finds a space in the previous character
-    while (line.charAt(start - 1).match(/[^,\s]/)) {
-        start--;
-    }
+  while (line.charAt(start - 1).match(/[^,\s]/)) {
+    start--;
+  }
 
     // if the cursor is in front of a space, return nothing
-    if (start == end) {
-        return;
-    }
+  if (start == end) {
+    return;
+  }
 
-    var from = CodeMirror.Pos(cursor.line, start);
-    var to = CodeMirror.Pos(cursor.line, end);
+  const from = CodeMirror.Pos(cursor.line, start);
+  const to = CodeMirror.Pos(cursor.line, end);
 
-    var list = [];
+  let list = [];
 
-    var state = editor.getTokenAt(from).state.state;
+  const state = editor.getTokenAt(from).state.state;
 
-    var completions = {
-        operators: ["Add", "Subt", "AddI", "Load", "LoadI", "Store", "StoreI", "Jump", "Skipcond", "JnS", "JumpI", "Clear", "Input", "Output", "Halt"],
-        origination: ["ORG"],
-        operand: ["DEC", "OCT", "HEX"]
-    };
+  const completions = {
+    operators: ['Add', 'Subt', 'AddI', 'Load', 'LoadI', 'Store', 'StoreI', 'Jump', 'Skipcond', 'JnS', 'JumpI', 'Clear', 'Input', 'Output', 'Halt'],
+    origination: ['ORG'],
+    operand: ['DEC', 'OCT', 'HEX'],
+  };
 
-    var nonOperandOperators = ["Clear", "Input", "Output", "Halt"];
+  const nonOperandOperators = ['Clear', 'Input', 'Output', 'Halt'];
 
-    switch (state) {
-        case ("start"):
-            Array.prototype.push.apply(list, completions.origination.map(function(x) {
-                return {
-                    text: x,
-                    className: "completion-origination"
-                };
-            }));
+  switch (state) {
+    case ('start'):
+      Array.prototype.push.apply(list, completions.origination.map((x) => {
+        return {
+          text: x,
+          className: 'completion-origination',
+        };
+      }));
             /* falls through */
-        case ("operator"):
-            Array.prototype.push.apply(list, completions.operators.map(function(x) {
-                return {
-                    text: x + (nonOperandOperators.indexOf(x) < 0 || containsForwardSlash ? ' ' : '\n'),
-                    className: "completion-operator"
-                };
-            }));
+    case ('operator'):
+      Array.prototype.push.apply(list, completions.operators.map((x) => {
+        return {
+          text: x + (nonOperandOperators.indexOf(x) < 0 || containsForwardSlash ? ' ' : '\n'),
+          className: 'completion-operator',
+        };
+      }));
             /* falls through */
-        case ("operand"):
-            Array.prototype.push.apply(list, completions.operand.map(function(x) {
-                return {
-                    text: x + ' ',
-                    className: "completion-operand"
-                };
-            }));
-    }
+    case ('operand'):
+      Array.prototype.push.apply(list, completions.operand.map((x) => {
+        return {
+          text: x + ' ',
+          className: 'completion-operand',
+        };
+      }));
+  }
 
-    if (state == "operand") {
+  if (state == 'operand') {
         // Also find labels for completion
-        for (var i = 0; i < editor.lineCount(); i++) {
-            var label = editor.getLine(i).match(/^(?:([^,\/]+),)/);
-            if (label) {
-                list.push({
-                    text: label[1] + '\n',
-                    className: "completion-label"
-                });
-            }
-        }
+    for (let i = 0; i < editor.lineCount(); i++) {
+      const label = editor.getLine(i).match(/^(?:([^,\/]+),)/);
+      if (label) {
+        list.push({
+          text: label[1] + '\n',
+          className: 'completion-label',
+        });
+      }
     }
+  }
 
-    list = list.filter(function(completion) {
-        var x = completion.text.toLowerCase();
-        var y = line.substring(start, end).toLowerCase();
-        return x.trim() != y.trim() && x.indexOf(y) === 0;
-    });
+  list = list.filter((completion) => {
+    const x = completion.text.toLowerCase();
+    const y = line.substring(start, end).toLowerCase();
+    return x.trim() != y.trim() && x.indexOf(y) === 0;
+  });
 
-    return {
-        list: list,
-        from: from,
-        to: to
-    };
+  return {
+    list,
+    from,
+    to,
+  };
 }
