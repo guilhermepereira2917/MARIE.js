6aa5c2ae1da01c09abb64241ee058fcac6d67437
lorderikir <ejia6@student.monash.edu>
2016-09-28 21:51:33 +1000

    various bug fixes

--- src/js/theme.js
+++ src/js/theme.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         $('#mode').attr('href','/css/styleDark.css');
         console.info("Switching to Dark Theme");
       }
-      $('#displayVersion').click(function(){w
+      $('#displayVersion').click(function(){
         $('#currentVersion').modal('toggle');
       });
 });
